fcafe77c49595b6540edf1159740cfa0d1411748
TudorRed <tudordn777@gmail.com>
2026-02-22 23:17:47 +0200

    Finished up to Chapter 2.4

--- Chapter_2/lesson3.cpp
+++ Chapter_2/lesson3.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 
+// Void functions (non-value returning functions)
+
+void returnNothing()
+{
+    
+
+   
+}
+
+int returnFive()
+{
+    return 5;
+}
 
 int main()
 {
+
+    returnNothing();
+    returnFive();
+
+    std::cout << returnFive();
+    std::cout << returnNothing();  
+
 
 
     return 0;
