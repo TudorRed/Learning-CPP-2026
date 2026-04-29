3e29534ea384f151583264b4702f552c59afde73
TudorRed <tudordn777@gmail.com>
2026-04-29 16:29:45 +0300

    Site keeps crashing, switching to an alternative source of learning

---
+++ Chapter_2/lesson13/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+
+
+    // Full implementation of the getUserInput function
+    int getUserInput()
+{
+    std::cout << "Enter an integer: ";
+    int input{};
+    std::cin >> input;
+
+    return input;
+
+}
+
+int main()
+{
+    // Get first number from user 
+    int value{ getUserInput() }; 
+    std::cout << value << "\n";
+    
+
+    // Get matehmatical operation from user 
+//  getMathematicalOperation();
+
+    // Get second number from user
+//  geUserInput();
+
+    // Calculate result
+//  calculateResult();
+
+    // Print result
+//  printResult();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
---
+++ Chapter_4/lesson1/main.cpp
@@ -0,0 +1,15 @@
+// 4.1 Introduction to fundamental data types 
+
+// the smallest unti of memory is a binary digit, also called a bit
+// memory is organzed into sequential untis called memory adresses 
+// a byte is comprised of 8 sequential bits 
+
+#include <iostream>
+
+
+int main()
+{
+
+
+    return 0;
+}
